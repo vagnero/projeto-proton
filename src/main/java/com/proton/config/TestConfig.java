6a673d365f4b6362ba6f7bfd18741af2ecedf1de
AudreyBergamine <audreybergamini@outlook.com>
2024-03-03 17:45:28 -0300

    Teste DEV Team

--- src/main/java/com/proton/config/TestConfig.java
+++ src/main/java/com/proton/config/TestConfig.java
@@ -2,6 +2,8 @@ package com.proton.config;
 
 import org.springframework.boot.CommandLineRunner;
 
+// Teste Audrey 
+
 public class TestConfig implements CommandLineRunner {
     @Override
 	public void run(String... args) throws Exception {
